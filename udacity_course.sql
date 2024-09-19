377dbb52b08c9a3ecffcfae2d8cdff353c4cff8f
chrisstaromana <r0mana.hendrix@gmail.com>
2024-09-18 22:19:22 -0400

    Update udacity_course.sql

--- udacity_course.sql
+++ udacity_course.sql
@@ -17,3 +17,19 @@ SELECT *
 FROM orders
 WHERE total < 500
 LIMIT 10;
+-- 1.28
+SELECT name, website, primary_poc
+FROM accounts
+WHERE name = 'Exxon Mobil';
+-- 1.30 
+SELECT id, (standard_amt_usd/total_amt_usd)*100 AS std_percent, total_amt_usd
+FROM orders
+LIMIT 10;
+-- 1.31 1
+SELECT id, account_id, standard_amt_usd/standard_qty AS unit_price 
+FROM orders
+LIMIT 10;
+-- 1.31 2
+SELECT id, account_id, poster_amt_usd/(standard_amt_usd + gloss_amt_usd + poster_amt_usd) * 100 AS percentage_of_revenue_from_poster_paper
+FROM orders
+LIMIT 10;
